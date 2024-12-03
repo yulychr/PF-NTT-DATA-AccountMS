f9adc4fe2a8ea1b5d20d8c0c854adc9c8b127ffd
yulychr <sandra_ch.r@hotmail.com>
2024-12-03 16:46:33 -0500

    chore: Remove unused code to improve maintainability

--- src/main/java/com/corebankingsystem/AccountMs/controller/AccountController.java
+++ src/main/java/com/corebankingsystem/AccountMs/controller/AccountController.java
@@ -5,7 +5,6 @@ import com.corebankingsystem.AccountMs.DTO.WithdrawalRequestDTO;
 import com.corebankingsystem.AccountMs.model.entity.Account;
 import com.corebankingsystem.AccountMs.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
--- src/main/java/com/corebankingsystem/AccountMs/service/impl/AccountServiceImpl.java
+++ src/main/java/com/corebankingsystem/AccountMs/service/impl/AccountServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
